b8923baec13020665ff1564770f1a3e1e98a815f
Victor Torres <torresvictor100@gmail.com>
2022-04-06 13:49:31 -0300

    inicio do curso

---
+++ src/main/java/com/curso/ionic/curso/ionic/CursoIonicApplication.java
@@ -0,0 +1,13 @@
+package com.curso.ionic.curso.ionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoIonicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/ionic/curso/ionic/CursoIonicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.ionic.curso.ionic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoIonicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
